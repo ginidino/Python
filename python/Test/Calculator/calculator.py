1addf20b4cfe7f6b19b20b415cbdfd7ccd110750
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-06-02 17:49:21 +0900

    Create calculator.py

---
+++ python/Test/Calculator/calculator.py
@@ -0,0 +1,19 @@
+# simple calculator only '+', '-', '*', '/'
+first = int(input('Type a fist number: '))
+second = int(input('Type a second number: '))
+
+# '+'
+sum = first + second
+print(first, '+', second, '=', sum)
+
+# '-'
+subtraction = first - second
+print(first, '-', second, '=', subtraction)
+
+# '*'
+multiply = first * second
+print(first, '*', second, '=', multiply)
+
+# '/'
+division = first / second
+print(first, '/', second, '=', division)

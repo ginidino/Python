a6a9493b4b43d1b19c5ef872a732673f1481775d
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-06-06 12:03:24 +0900

    Update test.py

--- python/Test/Math/test.py
+++ python/Test/Math/test.py
@@ -1,4 +1,6 @@
 # square: using pow function
+import math
+
 base = int(input('Type a base: '))
 power = int(input('Type a power: '))
 result = pow(base, power)
@@ -14,3 +16,21 @@ Rectangle = width * height      # Square
 print('Area of triangle = ', triangle)
 print('Area of square = ', Rectangle)
 print()
+
+# A code that receives an integer N between 3 and 6 and outputs
+# the sum of the interior angles of a regular N polygon and the size of one interior angle
+num = int(input('Type a integer between 3 and 6: '))
+sumAngle = 180 * (num - 2)          # The sum of the sizes of the n-square interior = 180° × (n - 2)
+intAngle = (180 * (num - 2)) / num  # The size of one interior angle of a regular n-gon = {180° × (n - 2)} ÷ n
+print('정',num, '각형의 내각의 합 = ', sumAngle,'°')
+print('정',num, '각형의 한 내각의 크기 = ', intAngle,'°')
+print()
+
+# Code to find the area of a circle by inputting the radius of the circle
+radius = int(input('Type a the radius of the circle: '))
+PI = math.pi
+print(PI)
+area = radius * radius * 3.14
+area1 = radius * radius * PI
+print('The area of the circle with ', radius, '= ', area)
+print('The area of the circle with ', radius, '= ', area1)

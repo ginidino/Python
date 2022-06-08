6d855fd74cd7eaf8b121355ad53686c9d8cecd6f
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-06-08 18:19:34 +0900

    Add files via upload

---
+++ Concept/Print/printTest.py
@@ -0,0 +1,20 @@
+print("Hello")  # Hello
+print("100")    # 100(String)
+print("%d" % 100)   # 100(number)
+
+print("100 + 100")    # 100 + 100(String)
+print("%d" % (100 + 100))   # 200(number)
+print()
+
+print("%d / %d = %0.1f" % (100, 200, 0.5), '\n')
+
+print("%d" % 123)
+print("%5d" % 123)
+print("%05d" % 123, '\n')
+
+print("%f" % 123.45)
+print("%7.1f" % 123.45)
+print("%7.3f" % 123.45, '\n')
+
+print("%s" % 'Python')
+print("%10s" % 'Python')

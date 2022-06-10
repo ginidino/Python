c98d19d86196d66c5fa17a38e6c45d2084b2330a
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-06-10 17:50:14 +0900

    Add files via upload

---
+++ Concept/variable/variableTest.py
@@ -0,0 +1,53 @@
+# declaration of a variable
+intVar, boolVar, floatVar, strVar = 0, True, 0.0, ""
+
+# The type( ) function
+print('The type( ) function')
+type(intVar), type(boolVar), type(floatVar), type(strVar)
+print(type(intVar),', ', type(boolVar),', ', type(floatVar),', ', type(strVar))
+print()
+
+# How to put a value in a variable
+print('How to put a value in a variable')
+boolVar = False
+intVar = 100
+floatVar = 123.45
+strVar = 'Hello'
+
+print(boolVar, intVar, floatVar, strVar, '\n')
+
+# Putting the value of a variable into a variable
+print('Putting the value of a variable into a variable')
+var2 = 200
+var1 = var2
+print('var1: ', var1)
+print('var2: ', var2)
+print()
+
+# Put the calculation result into a variable
+print('Put the calculation result into a variable')
+var3 = 100 + 100
+print('var3: ', var3)
+print()
+
+# Putting calculations of variables and numbers into variables
+print('Putting calculations of variables and numbers into variables')
+var4 = var1 + 100
+print('var4: ', var4)
+print()
+
+# How to substitute consecutive values
+print('How to substitute consecutive values')
+var5 = var6 = var7 = var8 = 100
+print('var5: ', var5)
+print('var6: ', var6)
+print('var7: ', var7)
+print('var8: ', var8)
+print()
+
+# A method of substituting calculation results into own values
+print('A method of substituting calculation results into own values')
+val1 = var1 + 200
+print('var1: ', var1)
+print()
+

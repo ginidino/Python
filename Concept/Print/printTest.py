d085a91fee254bc5a2141fbc29a2ba9af0a387e1
Injae Lee <94044443+ginidino@users.noreply.github.com>
2022-06-10 17:14:31 +0900

    Update printTest.py

--- Concept/Print/printTest.py
+++ Concept/Print/printTest.py
@@ -1,3 +1,4 @@
+# How to use the print( ) function
 print("Hello")  # Hello
 print("100")    # 100(String)
 print("%d" % 100)   # 100(number)
@@ -17,4 +18,20 @@ print("%7.1f" % 123.45)
 print("%7.3f" % 123.45, '\n')
 
 print("%s" % 'Python')
-print("%10s" % 'Python')
+print("%10s" % 'Python', '\n')
+
+# Using the format( ) function
+print("%d %5d %05d" % (123, 123, 123))
+print("{0:d} {1:5d} {2:05d}".format(123, 123, 123))
+print()
+
+# various escape characters
+print('one line. and one line.')
+print('one line. \nand one line', '\n')
+
+print('\npractice \nchanging the lines')
+print('\tpractice \tTab key')
+print("\"The effect of\" highlighting letters1")
+print("\'The effect of\' highlighting letters2")
+print('\\\\\\ print 3 backslash')
+print(r"print \n \t \" \\ as it is")
